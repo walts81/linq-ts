1e909a7da8b2018fe41f37ca6b8dc69e2510f862
Josh Walters <josh.walters@outlook.com>
2022-01-06 17:25:13 +0000

    updated sum to exclude non-numeric

--- src/sum.spec.ts
+++ src/sum.spec.ts
@@ -6,6 +6,10 @@ describe('linq.sum', () => {
   const empty: any[] = [];
   const collection = [1, 2, 3, 4, 5];
   const collection2 = [{ id: 1 }, { id: 2 }, { id: 3 }, { id: 4 }, { id: 5 }];
+  const nullCollection = [...collection, null];
+  const undefinedCollection = [...collection, undefined];
+  const blankCollection = [...collection, ''];
+  const alphaCollection = [...collection, 'abc'];
 
   it('should return zero when array is empty', () => {
     const result = empty.sum();
@@ -19,6 +23,26 @@ describe('linq.sum', () => {
 
   it('should return sum when selector is provided', () => {
     const result = collection2.sum(x => x.id);
+    expect(result).to.equal(15);
+  });
+
+  it('should exclude null', () => {
+    const result = nullCollection.sum();
+    expect(result).to.equal(15);
+  });
+
+  it('should exclude undefined', () => {
+    const result = undefinedCollection.sum();
+    expect(result).to.equal(15);
+  });
+
+  it('should exclude blank', () => {
+    const result = blankCollection.sum();
+    expect(result).to.equal(15);
+  });
+
+  it('should exclude alpha', () => {
+    const result = alphaCollection.sum();
     expect(result).to.equal(15);
   });
 });
--- src/sum.ts
+++ src/sum.ts
@@ -4,15 +4,22 @@ declare global {
   }
 }
 
-Array.prototype.sum = function<T>(expression?: (item: T) => number): number {
+Array.prototype.sum = function <T>(expression?: (item: T) => number): number {
   return sum(this, expression);
 };
 
+const getSafeNumber = (val: any) => {
+  if (!val) return 0;
+
+  const numVal = Number(val);
+  return isNaN(numVal) ? 0 : numVal;
+};
+
 export default function sum<T>(collection: T[], expression?: (item: T) => number): number {
-  const exp: ((x: any) => number) = expression || (x => x);
+  const exp: (x: any) => number = expression || (x => x);
   let amount = 0;
   for (const x of collection) {
-    amount += exp(x);
+    amount += getSafeNumber(exp(x));
   }
   return amount;
 }
